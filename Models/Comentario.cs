ced009585059070f9c4ecaa406ff69f4a21f5b54
Maro de Melo <marodemelo10@gmail.com>
2021-02-14 09:42:03 -0300

    Changed type name of some properties

--- Models/Comentario.cs
+++ Models/Comentario.cs
@@ -6,9 +6,12 @@ namespace Blog.Models
     public class Comentario
     {
         public int Id { get; set; }
-        
+
+        [StringLength(100)]
+                
         public string Autor { get; set; }
         
+        [Column(TypeName="TEXT")]        
         public string Conteudo { get; set; }
         
         public DateTime Data { get; set; }
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
     public class Post
     {
         public int Id { get; set; }
+        [StringLength(100)]
         
         public string Titulo { get; set; }
+        [Column(TypeName="TEXT")]
         
         public string Texto { get; set; }
         
