367b15437c921af5a8ef1129ded2866388632cd6
Maro de Melo <marodemelo10@gmail.com>
2021-02-14 09:58:14 -0300

    Created PostService class

--- Migrations/20210214124330_ChangeVarchar.Designer.cs
+++ Migrations/20210214124330_ChangeVarchar.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Blog.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20210214124330_ChangeVarchar")]
+    partial class ChangeVarchar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Comentario.cs
+++ Models/Comentario.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
@@ -10,7 +11,7 @@ namespace Blog.Models
         [StringLength(100)]
                 
         public string Autor { get; set; }
-        
+
         [Column(TypeName="TEXT")]        
         public string Conteudo { get; set; }
         
